3a6b85fda4a4527085f5ec15470e63527e4aa9ce
Isaitamilselvan <161854565+IsaiTamilselvan@users.noreply.github.com>
2026-01-09 19:56:01 +0600

    Create oops1.java

---
+++ oops/oops1.java
@@ -0,0 +1,40 @@
+class Student{
+    int rno;
+    int marks;
+    String level;
+}
+public class Oops1{
+    public static void main(java.lang.String[] args){
+    
+        
+        // obj create for class student 
+        Student hari = new Student();
+        Student mani = new Student();
+        Student mark = new Student();
+        Student peter = new Student();
+        
+        // value assign
+        hari.rno=14;
+        hari.level="good";
+        hari.marks=86;
+
+        // value assign
+        peter.rno=15;
+        peter.level="improvement needed";
+        peter.marks=86;
+
+        // value assign
+        mark.rno=35;
+        mark.level="not good";
+        mark.marks=76;
+
+        // value assign
+        mani.rno=34;
+        mani.level="good";
+        mani.marks=66;
+
+
+        //retrive value from data
+
+    }
+}
