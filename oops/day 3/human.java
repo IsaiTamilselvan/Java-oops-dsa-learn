b466a158fc00eeaf37f1700a8262790047244d29
Isaitamilselvan <161854565+IsaiTamilselvan@users.noreply.github.com>
2026-01-12 21:06:13 +0600

    Update human.java

--- oops/day 3/human.java
+++ oops/day 3/human.java
@@ -1,1 +1,27 @@
+package com;
+
+public class human{
+    int age;
+    String name;
+    boolean married;
+
+    static long population;
+
+    public human(int age,String name,boolean married){
+        this.age=age;
+        this.name=name;
+        this.married=married;
+
+        human.population+=1;
+
+        System.out.println("new human data is added");
+        System.out.println("current population"+human.population);
+    }
+
+    public void getpeopleinfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+        System.out.println(this.married);
+    }
+}
 
